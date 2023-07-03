325029d9a460c1a4ed56eb160229a6654fe7d625
Haoran <jhr12578@g.ucla.edu>
2023-07-03 13:55:44 -0700

    add utils

---
+++ utils/dataset.py
@@ -0,0 +1,39 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.utils.data as data
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+
+class_labels = {
+    0: 'Airplane',
+    1: 'Automobile',
+    2: 'Bird',
+    3: 'Cat',
+    4: 'Deer',
+    5: 'Dog',
+    6: 'Frog',
+    7: 'Horse',
+    8: 'Ship',
+    9: 'Truck'
+}
